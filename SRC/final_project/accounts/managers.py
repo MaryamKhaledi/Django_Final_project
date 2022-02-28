a29e96218b0227cdee20563de6c127881a2df911
Maryam <khaledi.maryam74@gmail.com>
2022-02-28 12:20:25 +0300

    layout

--- SRC/final_project/accounts/managers.py
+++ SRC/final_project/accounts/managers.py
@@ -8,16 +8,16 @@ class UserManager(BaseUserManager):
 
         if not username:
             raise ValueError('accounts must have email')
-        # username = username+"@zyo"
-        user = self.model(username=username,phone_number=phone_number,  first_name=first_name, last_name=last_name,
+        # username = username+"@eml"
+        user = self.model(username=username, phone_number=phone_number, first_name=first_name, last_name=last_name,
                           birth_date=birth_date, gender=gender, country=country)
         user.set_password(password)
 
         user.save()
         return user
 
-    # def create_superuser(self, phone_number, email, password):
-    #     accounts = self.create_user(phone_number, email, password)
-    #     accounts.is_admin = True
-    #     accounts.save(using=self._db)
-    #     return
+    def create_superuser(self, phone_number, email, password):
+        accounts = self.create_user(phone_number, email, password)
+        accounts.is_admin = True
+        accounts.save(using=self._db)
+        return
